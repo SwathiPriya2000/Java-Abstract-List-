3eec0f36a7b8f1e51774e5d5976a00e2cf4a3757
SwathiPriya2000 <63891711+SwathiPriya2000@users.noreply.github.com>
2020-10-12 22:29:32 +0530

    Add files via upload

---
+++ AbstractList.java
@@ -0,0 +1,25 @@
+// Java code to illustrate AbstractList
+import java.util.*;
+ 
+public class AbstractList {
+    public static void main(String args[])
+    {
+ 
+        // Creating an empty AbstractList
+        ArrayList<String> list = new ArrayList<String>();
+ 
+        // Use add() method to add elements in the list
+        list.add("Live");
+        list.add("Laugh");
+        list.add("Love");
+        list.add("100");
+        list.add("200");
+ 
+        // Displaying the AbstractList
+        System.out.println("AbstractList:" + list);
+    }
+}
+
+
+
+
